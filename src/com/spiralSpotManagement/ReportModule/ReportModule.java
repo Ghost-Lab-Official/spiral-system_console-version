8374c2a017bef44200e08424d725d78657238ee9
MikeManzi <manzimike378@gmail.com>
2021-01-29 20:46:39 +0200

    Added the admin dashboard

--- src/com/spiralSpotManagement/ReportModule/ReportModule.java
+++ src/com/spiralSpotManagement/ReportModule/ReportModule.java
@@ -1,4 +1,18 @@
 package com.spiralSpotManagement.ReportModule;
 
+import java.util.Scanner;
+
 public class ReportModule {
+    public static void reportDashboard() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t\t\t============================================= ");
+        System.out.println("\t\t\t=                ADMIN DASHBOARD            = ");
+        System.out.println("\t\t\t============================================= ");
+        System.out.println("\t\t\t|| 1.  Home                                ||");
+        System.out.println("\t\t\t|| 2.  Spot Management                     ||");
+        System.out.println("\t\t\t|| 3.  Users Management                    ||");
+        System.out.println("\t\t\t|| 4.  Settings                            ||");
+        System.out.println("\t\t\t============================================ ");
+        String choice = scanner.nextLine();
+    }
 }
