08cb59fa906ac0a80e06f510070c6e069e71d5bb
Muhodari <sagemuho@gmail.com>
2021-01-30 07:49:50 +0200

    fetching users done another task over

--- src/com/spiralSpotManagement/Main.java
+++ src/com/spiralSpotManagement/Main.java
@@ -1,12 +1,17 @@
 package com.spiralSpotManagement;
 
 import com.spiralSpotManagement.DbConnection.CloudStorageConnection;
+import com.spiralSpotManagement.UsersModule.UsersModule;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         System.out.println("HELLO SPIRAL SYSTEM\n\n ");
         CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
-        cloudStorageConnection.checkDbWorking(cloudStorageConnection.getConnection());
+//        cloudStorageConnection.checkDbWorking(cloudStorageConnection.getConnection());
+
+        UsersModule usersModule=new UsersModule();
+         usersModule.getUsersList(cloudStorageConnection.getConnection());
+
     }
 }
--- src/com/spiralSpotManagement/UsersModule/UsersModule.java
+++ src/com/spiralSpotManagement/UsersModule/UsersModule.java
@@ -1,8 +1,39 @@
 package com.spiralSpotManagement.UsersModule;
 import com.spiralSpotManagement.DbConnection.CloudStorageConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class UsersModule {
     CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
+
+
+
+
+
+//view users list function  done by muhodari
+    public void getUsersList(Connection connection)throws Exception{
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("select  * from users_table");
+        System.out.println(" -------------------------- USERS TABLE------------------------- \n\n");
+        System.out.println(" user_id\t\tfirst_name\t\tlast_Name\t\tEmail\t\t\t\t\tGender\t\tLocation");
+        System.out.println(" --------------------------------------------------------------------------------------------------------------------------------- ");
+
+        while (rs.next()) {
+            int id = rs.getInt("user_id");
+            String firstName = rs.getString("first_name");
+            String lastName = rs.getString("last_name");
+            String email=rs.getString("email");
+            String gender=rs.getString("gender");
+            String location=rs.getString("location");
+            System.out.println(id+"\t\t\t\t"+firstName+"\t\t"+lastName+"\t\t\t"+email+"\t\t"+gender+"\t\t"+location);
+            System.out.println(" ------------------------------------------------------------------------------------------------------------------------------------ ");
+        }
+    }
+
+
+
+
 
 
 }
