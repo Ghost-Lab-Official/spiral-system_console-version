518732ab7e27095240187f384decc1b3ba854985
Muhodari <sagemuho@gmail.com>
2021-01-30 07:02:01 +0200

    my first task of user database design endes soo verry happy

--- src/com/spiralSpotManagement/UsersModule/UsersModule.java
+++ src/com/spiralSpotManagement/UsersModule/UsersModule.java
@@ -1,4 +1,8 @@
 package com.spiralSpotManagement.UsersModule;
+import com.spiralSpotManagement.DbConnection.CloudStorageConnection;
 
 public class UsersModule {
+    CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
+
+
 }
