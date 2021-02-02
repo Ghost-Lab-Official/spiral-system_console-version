60197a552f046ed3a282bb9dd8f04f6bd540ba20
Bethiane <ubethiane07@gmail.com>
2021-02-02 10:10:45 -0800

    Updated mockup

--- src/com/spiralSpotManagement/SearchModule/DisplaySpot.java
+++ src/com/spiralSpotManagement/SearchModule/DisplaySpot.java
@@ -1,16 +1,19 @@
 package com.spiralSpotManagement.SearchModule;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DisplaySpot {
+    public static Scanner scanner = new Scanner(System.in);
+    private  static  String toContinue;
 
-    public static int filter(){
+    public static int filter() {
         System.out.println("The filtered result is:\n");
         return 0;
     }
 
     public static int levelOne() {
-        filter();
-        Scanner scanner=new Scanner(System.in);
+        //filter();
+
         System.out.println("|---------------SPOTS SEARCHING----------------|");
         System.out.println("|-----Kigali Convention Center ~Text Search----|\n");
         System.out.println("|1.Kigali Convention Center located in Kigali|");
@@ -18,30 +21,41 @@ public class DisplaySpot {
         System.out.println("|3.Kigali Convention Center kitchen|");
         System.out.println("|4.Radisson Blu Hotel|\n");
         System.out.println("Enter your specific research");
-        int special=scanner.nextInt();
-        scanner.close();
+        int special = scanner.nextInt();
         return special;
     }
 
-        public static void levelTwo() {
-            filter();
-            Scanner scanner=new Scanner(System.in);
+    public static void levelTwo() {
+        // filter();
             System.out.println("|-------------------------------------------------------|");
             System.out.println("|---------------    SPOTS SEARCHING       --------------|");
             System.out.println("|-------------------------------------------------------|");
-            System.out.println("|Name:----------   Radisson Blu Hotel     --------------|");
-            System.out.println("|Views:---------          102             --------------|");
-            System.out.println("|Location:------      Kigali,Rwanda       --------------|");
-            System.out.println("|Description:--- It is what it is niki niki! -----------|");
+            System.out.println("\t\t Name:Radisson Blu Hotel");
+            System.out.println("\t\t Views:102");
+            System.out.println("\t\t Location:Kigali,Rwanda");
+            System.out.println("\t\t Description:It is what it is niki niki!");
             System.out.println("|-------------------------------------------------------|");
             System.out.println("|-------------------------------------------------------|");
-        }
+            //dealWithDashboard();
 
-        public static void main(String[] args){
-            int nextLevel=levelOne();
-            if(nextLevel==4){
-             levelTwo();
-            }
+    }
 
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        do {
+        int nextLevel = levelOne();
+        if (nextLevel == 4) {
+            levelTwo();
         }
+            System.out.println("Byarangiye birongera biratangira");
+            //userChoice = levelOne();
+            System.out.println(" Do you want to continue Y/N ? ");
+            toContinue = scanner.next();
+            //  levelOne();
+        } while (toContinue.equals("Y") || toContinue.equals("y")
+        );
+    }
 }
