ada4e9b80db964dcd5ffea854a7cf8843074298e
NAC-ANGE <constance.nimuhire@gmail.com>
2021-02-03 07:45:33 +0200

    my second commit

--- src/com/spiralSpotManagement/Main.java
+++ src/com/spiralSpotManagement/Main.java
@@ -1,7 +1,6 @@
 package com.spiralSpotManagement;
 
 import com.spiralSpotManagement.DbConnection.CloudStorageConnection;
-import com.spiralSpotManagement.ReportModule.ReportModule;
 
 public class Main {
 
@@ -9,6 +8,8 @@ public class Main {
         System.out.println("HELLO SPIRAL SYSTEM\n\n ");
         CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
         cloudStorageConnection.checkDbWorking(cloudStorageConnection.getConnection());
+
+
 
     }
 
