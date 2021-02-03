ff36e30faed847e6a5ce05868a057fda034c855d
Cyebukayire <peacecyebukayire@gmail.com>
2021-02-03 17:36:37 +0200

    New updates: Creating Spot

--- src/com/spiralSpotManagement/SpotModule/CreateSpot.java
+++ src/com/spiralSpotManagement/SpotModule/CreateSpot.java
@@ -24,48 +24,45 @@ public class CreateSpot {
 
 
 
-    private static final String InsertSql = "INSERT INTO spots (spot) VALUES(?)";
-    private static final String UpdateSql = "UPDATE spots SET spot_entity=? WHERE spot_id=?";
-    private static final String deleteSQL = "DELETE FROM spots where spot_id=?";
+    private static final String InsertSql = "INSERT INTO Spot_table (user_id, category_id, location_id, spot_name, spot_description, views, viewers, rates, registration_date, update_date, status) VALUES(?)";
+    private static final String UpdateSql = "UPDATE Spot_table SET spot_entity=? WHERE spot_id=?";
+    private static final String deleteSQL = "DELETE FROM Spot_table where spot_id=?";
 
 
 
 //    Insert spot function
 
     public static void insertSpot(Connection connection) throws SQLException{
-
     }
 
 
 
     //  select Spot function
     public static void selectSpot(Connection connection)throws Exception{
-
     }
 
 
     //    Update Spot function
     public static void UpdateSpot(Connection connection) throws Exception{
-
     }
 
     //delete function
     public static void deleteSpot(Connection connection) throws Exception{
-
     }
 
 
     //    main function
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("==================================");
-        System.out.println("||\t\tSpot  Section\t\t\t||\n");
+        System.out.println("==============================");
+        System.out.println("||\t\tSpot  Section\t\t||");
+        System.out.println("==============================\n");
         System.out.println("||\t\t1.Create Spot\t\t||\n");
-        System.out.println("||\t\t2.Edit Spot  \t\t||\n");
-        System.out.println("||\t\t3.View Spot\t\t||\n");
+        System.out.println("||\t\t2.Edit Spot\t\t\t||\n");
+        System.out.println("||\t\t3.View Spot\t\t\t||\n");
         System.out.println("||\t\t4.Delete Spot\t\t||\n");
-        System.out.println("||\t\t5.Exit           \t\t||\n");
-        System.out.println("==================================");
+        System.out.println("||\t\t5.Exit         \t\t||\n");
+        System.out.println("==============================");
         String choose =input.nextLine();
 
         CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
