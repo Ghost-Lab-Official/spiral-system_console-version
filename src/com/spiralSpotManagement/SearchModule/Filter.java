56d08c989cefbc73b234a502d14df739fe843a15
oreste-abizera <oresteabizera11@gmail.com>
2021-02-04 14:32:39 +0200

    Improving Search Spot and removing the deleted Spots

--- src/com/spiralSpotManagement/SearchModule/Filter.java
+++ src/com/spiralSpotManagement/SearchModule/Filter.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Filter {
 
+    //Author: Oreste Abizera
+    //Description: filter searches by spot
     public void spotFilter(Statement stmt) throws Exception {
         Scanner scanInput=new Scanner(System.in);
         String searchKey;
@@ -12,7 +14,7 @@ public class Filter {
         System.out.println("\n\t\t\t Search a spot: ");
         searchKey = scanInput.nextLine();
 
-        String sql = "SELECT * from Spot_table WHERE spot_name LIKE '%"+searchKey+"%' OR spot_description LIKE '%"+searchKey+"%' ORDER BY registration_date DESC";
+        String sql = "SELECT * from Spot_table WHERE spot_name LIKE '%"+searchKey+"%' OR spot_description LIKE '%"+searchKey+"%' AND status=1 ORDER BY registration_date DESC";
 
         ResultSet rs = stmt.executeQuery(sql);
         Boolean found = false;
@@ -20,7 +22,12 @@ public class Filter {
         while (rs.next()){
             found = true;
             results++;
-            System.out.println(rs.getString("spot_name") + ": " + rs.getString("spot_description"));
+            String name = rs.getString("spot_name");
+            String description = rs.getString("spot_description");
+            Integer views = rs.getInt("views");
+            Integer rates = rs.getInt("rates");
+            System.out.println(results + ". " + name + ": " + description + "  " + views + " views" + "   " + rates + " ratings");
+            System.out.println("========================================================\n");
         }
 
         if(!found){
