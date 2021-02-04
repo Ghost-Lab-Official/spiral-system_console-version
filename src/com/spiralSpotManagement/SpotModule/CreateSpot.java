f7a4fb5e90fdec922351884afb1677ff140df730
Cyebukayire <peacecyebukayire@gmail.com>
2021-02-04 07:06:10 +0200

    Done!

--- src/com/spiralSpotManagement/SpotModule/CreateSpot.java
+++ src/com/spiralSpotManagement/SpotModule/CreateSpot.java
@@ -11,8 +11,8 @@ public class CreateSpot {
 //    private static int spotId;
     private static String spot_id, user_id, category_id, location_id, spot_name, spot_description, views, viewers, rates, registration_date, update_date, status;
 
-    public CreateSpot(String user_id, String category_id, String location_id, String spot_name, String spot_description, String registration_date, String status, String spot_id){
-        this.user_id=user_id; this.category_id=category_id; this.location_id=location_id; this.spot_name=spot_name; this.spot_description=spot_description; this.registration_date=registration_date; this.status=status; this.spot_id=spot_id;
+    public CreateSpot(String user_id, String category_id, String location_id, String spot_name, String spot_description, String date, String status, String spot_id){
+        this.user_id=user_id; this.category_id=category_id; this.location_id=location_id; this.spot_name=spot_name; this.spot_description=spot_description; this.registration_date=date; this.update_date=date; this.status=status; this.spot_id=spot_id;
     }
 
     public String getSpotId() { return spot_id; }
@@ -36,6 +36,9 @@ public class CreateSpot {
     public String getRegistration_date() { return registration_date; }
     public void setRegistration_date(String registration_date) { this.registration_date = registration_date; }
 
+    public String getUpdate_date() { return update_date; }
+    public void setUpdate_date(String update_date) { this.update_date = update_date; }
+
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 
@@ -44,16 +47,15 @@ public class CreateSpot {
 
 
     private static final String InsertSql = "INSERT INTO Spot_table (user_id, category_id, location_id, spot_name, spot_description, registration_date, status, spot_id) VALUES(?,?,?,?,?,?,?,?)";
-    private static final String UpdateSql = "UPDATE Spot_table SET category_id=?, location_id=?, spot_name=?, spot_description=? WHERE spot_id=?";
-    private static final String deleteSQL = "DELETE FROM Spot_table where spot_id=?";
+    private static final String UpdateSql = "UPDATE Spot_table SET user_id=?, category_id=?, location_id=?, spot_name=?, spot_description=?, update_date=?, status=? WHERE spot_id=?";
+    private static final String deleteSQL = "UPDATE Spot_table SET status=0 where spot_id=?";
 
 
 
 //    Insert spot function
 
     public static void insertSpot(Connection connection) throws SQLException{
-        String spot_id="6", user_id="1", category_id="45", location_id="5655", spot_name="Computer", spot_description="Balck and brown table found in Ouuaagaadouuguuu", registration_date="2021-01-31", status="1";
-        Scanner scan = new Scanner(System.in);
+        String spot_id="8", user_id="1", category_id="45", location_id="5655", spot_name="Computer", spot_description="Balck and brown table found in Ouuaagaadouuguuu", registration_date="2021-01-31", status="1";
         CreateSpot spot = new CreateSpot(user_id, category_id, location_id, spot_name, spot_description, registration_date, status, spot_id);
         try (PreparedStatement preparedStatement = connection.prepareStatement(InsertSql)){
             preparedStatement.setString(1,spot.getUserId());
@@ -64,7 +66,6 @@ public class CreateSpot {
             preparedStatement.setString(6,spot.getRegistration_date());
             preparedStatement.setString(7,spot.getStatus());
             preparedStatement.setString(8,spot.getSpotId());
-//            preparedStatement.setString(1,spot.getUserId());
             preparedStatement.execute();
             System.out.println(preparedStatement);
             System.out.println("\nInserted New Spot Successfully!\n");
@@ -78,15 +79,20 @@ public class CreateSpot {
     public static void UpdateSpot(Connection connection) throws Exception{
 
         Scanner scan = new Scanner(System.in);
-        String user_id="1", category_id="", location_id="", spot_name="", spot_description="", views="", viewers="", rates="", registration_date="", update_date="", status="";
-//        CreateSpot spot = new CreateSpot(user_id, category_id, location_id, spot_name, spot_description, views, viewers, rates, registration_date, update_date, status);
+        String spot_id="6", user_id="1", category_id="45", location_id="5655", spot_name="Table", spot_description="Black and brown table found in Ouuaagaadouuguuu", update_date="2021-01-31", status="1";
+        CreateSpot spot = new CreateSpot(user_id, category_id, location_id, spot_name, spot_description, update_date, status, spot_id);
         try (PreparedStatement sql = connection.prepareStatement(UpdateSql)){
-            sql.setString(1,category_id);
-            sql.setString(2,location_id);
-            sql.setString(3,spot_name);
-            sql.setString(4,spot_description);
+            sql.setString(1,spot.getUserId());
+            sql.setString(2,spot.getSpotCategory());
+            sql.setString(3,spot.getSpotLocation());
+            sql.setString(4,spot.getSpotName());
+            sql.setString(5,spot.getSpot_description());
+            sql.setString(6,spot.getUpdate_date());
+            sql.setString(7,spot.getStatus());
+            sql.setString(8,spot.getSpotId());
             sql.execute();
             System.out.println(sql);
+            System.out.println("\nUpdated Spot Successfully!\n");
         }catch (SQLException e){
             e.printStackTrace();
         }
