321932967c08affa1b5263cdc64c113ac003356b
hirwablessing <hirwablessin7@gmail.com>
2021-02-04 13:37:19 +0200

    New: Add description of classes and methods

--- src/com/spiralSpotManagement/SearchModule/SearchByPopularity.java
+++ src/com/spiralSpotManagement/SearchModule/SearchByPopularity.java
@@ -7,67 +7,57 @@ import java.util.Scanner;
 
 import com.spiralSpotManagement.DbConnection.CloudStorageConnection;
 
+//Authors: by Blessing and Izere Kerie
+//This class is used to fetch recent searched query from the search history table and then return top 10 most popular
+//searches. Popular searches will be determined by searchquery with most ratings,views,and most searched query.
 public class SearchByPopularity {
 
 	public static ArrayList<String> popularityByRatesArray(java.sql.Connection connection) throws SQLException{
 		ArrayList<String> spots=new ArrayList<String>();
-<<<<<<< HEAD
 		String SelectRatesquery="select *from Spot_table order by rates desc limit 5";
-	  String SelectViewsquery="select *from Spot_table order by views desc limit 5";
-		 PreparedStatement ptRates=connection.prepareStatement(SelectRatesquery);
-		 PreparedStatement ptViews=connection.prepareStatement(SelectViewsquery);
-		 ResultSet Ratesresults=ptRates.executeQuery();
-	     ResultSet Viewsresults=ptViews.executeQuery();
-
-		 while( Ratesresults.next()) {
-			 String spotName= Ratesresults.getString("spot_name");
-			if(!spots.contains(spotName)) {
-				 spots.add(spotName);
-			}
-					 
-		 }
-		 while(  Viewsresults.next() ) {
-			 String spotName= Viewsresults.getString("spot_name");
-
-			 
-			 if(!spots.contains(spotName)) {
-				 spots.add(spotName);
-			}	 
-		 }
-		
-		 return spots;
-=======
-		String SelectRatesquery="select * from Spot_table order by rates desc limit 5";
-		PreparedStatement pt=connection.prepareStatement(SelectRatesquery);
-		ResultSet results=pt.executeQuery();
+		String SelectViewsquery="select *from Spot_table order by views desc limit 5";
 		String SelectMostSearchedQuery = "SELECT searched_query FROM searchHistory GROUP BY searched_query ORDER BY COUNT(searched_query) DESC LIMIT 5";
+		PreparedStatement ptRates=connection.prepareStatement(SelectRatesquery);
+		PreparedStatement ptViews=connection.prepareStatement(SelectViewsquery);
 		PreparedStatement sq = connection.prepareStatement(SelectMostSearchedQuery);
+		ResultSet Ratesresults=ptRates.executeQuery();
+		ResultSet Viewsresults=ptViews.executeQuery();
+		ResultSet searchResults = sq.executeQuery();
 
-		ResultSet searcResults = sq.executeQuery();
+		while( Ratesresults.next()) {
+			String spotName= Ratesresults.getString("spot_name");
+			if(!spots.contains(spotName)) {
+				spots.add(spotName);
+			}
+
+		}
+		while(  Viewsresults.next() ) {
+			String spotName= Viewsresults.getString("spot_name");
 
 
-		while(results.next()) {
-
-			String spotName = results.getString("spot_name");
-			spots.add(spotName);
+			if(!spots.contains(spotName)) {
+				spots.add(spotName);
+			}
 		}
 
-		while (searcResults.next()){
+		while (searchResults.next()){
 
-			String searchedSpot = searcResults.getString("searched_query");
+			String searchedSpot = searchResults.getString("searched_query");
 			spots.add(searchedSpot);
 		}
 
 		return spots;
->>>>>>> 7e1f16bd6df32ea8f62acdea0290ccbe8c5dce73
+
 	}
 
+	//Method used to display popular spots in the console
 	public static void DisplayPopularSpots(ArrayList<String> popularSpots) {
 		for(int i=0;i<popularSpots.size();i++) {
 			System.out.println(i+1+"."+popularSpots.get(i));
 		}
 	}
 
+	//Main file of the program
 	public static void main(String[] args) throws Exception {
 
 		Scanner scanner=new Scanner(System.in);
@@ -75,6 +65,7 @@ public class SearchByPopularity {
 		ArrayList<String> popularSpots= new ArrayList<String>();
 		System.out.print("==== Search by popularity =====\n");
 
+		//connect to the db
 		CloudStorageConnection cloudStorageConnection = new CloudStorageConnection();
 		// cloudStorageConnection.checkDbWorking(cloudStorageConnection.getConnection());
 		popularityByRatesArray(cloudStorageConnection.getConnection());
