057b0ee8b54a4db40151d080c155118ff2308810
Bethiane <ubethiane07@gmail.com>
2021-02-04 04:47:48 -0800

    version

--- src/com/spiralSpotManagement/SearchModule/DisplaySpot.java
+++ src/com/spiralSpotManagement/SearchModule/DisplaySpot.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 public class DisplaySpot {
     public static Scanner scanner = new Scanner(System.in);
     private  static  String toContinue;
-
-    public static int filter() {
-        System.out.println("The filtered result is:\n");
-        return 0;
-    }
+    private static int special;
+    private static int filtered;
 
     public static int levelOne() {
-        //filter();
-
         System.out.println("|---------------SPOTS SEARCHING----------------|");
         System.out.println("|-----Kigali Convention Center ~Text Search----|\n");
         System.out.println("|1.Kigali Convention Center located in Kigali|");
@@ -21,12 +16,25 @@ public class DisplaySpot {
         System.out.println("|3.Kigali Convention Center kitchen|");
         System.out.println("|4.Radisson Blu Hotel|\n");
         System.out.println("Enter your specific research");
-        int special = scanner.nextInt();
+        special = scanner.nextInt();
         return special;
+
+    }
+
+    public static int filter() {
+        System.out.println("The filtered result is:\n");
+        System.out.println("Enter the query of your choice");
+        filtered = scanner.nextInt();
+            if (special != filtered) {
+                System.out.println("Invalid input");
+            } else if (special == filtered) {
+                levelTwo();
+            }
+
+        return filtered;
     }
 
     public static void levelTwo() {
-        // filter();
             System.out.println("|-------------------------------------------------------|");
             System.out.println("|---------------    SPOTS SEARCHING       --------------|");
             System.out.println("|-------------------------------------------------------|");
@@ -40,22 +48,19 @@ public class DisplaySpot {
 
     }
 
-
-
-
-
     public static void main(String[] args) throws IOException {
-        do {
-        int nextLevel = levelOne();
-        if (nextLevel == 4) {
-            levelTwo();
-        }
-            System.out.println("Byarangiye birongera biratangira");
-            //userChoice = levelOne();
-            System.out.println(" Do you want to continue Y/N ? ");
-            toContinue = scanner.next();
-            //  levelOne();
-        } while (toContinue.equals("Y") || toContinue.equals("y")
-        );
+    do{
+
+    levelOne();
+    filter();
+        System.out.println("Byarangiye birongera biratangira");
+        //userChoice = levelOne();
+        System.out.println(" Do you want to continue Y/N ? ");
+        toContinue = scanner.next();
+        //  levelOne();
     }
-}
+    while (toContinue.equals("Y") || toContinue.equals("y"));
+
+        }
+
+    }
