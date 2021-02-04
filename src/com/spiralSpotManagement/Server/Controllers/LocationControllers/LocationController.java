4266a41d38ccb2b2abbcc2d0339a70829600600a
ntwari-egide <ntwariegide2@gmail.com>
2021-02-04 10:23:44 -0800

    Constructed the new format of controller

---
+++ src/com/spiralSpotManagement/Server/Controllers/LocationControllers/LocationController.java
@@ -0,0 +1,4 @@
+package com.spiralSpotManagement.Server.Controllers.LocationControllers;
+
+public class LocationController {
+}
---
+++ src/com/spiralSpotManagement/Server/Controllers/SpotControllers/SpotController.java
@@ -0,0 +1,4 @@
+package com.spiralSpotManagement.Server.Controllers.SpotControllers;
+
+public class SpotController {
+}
