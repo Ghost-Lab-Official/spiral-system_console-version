ad39aa3ec8182d326c62ece0d77ece567f0c4fba
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-02-05 08:42:57 +0200

    added the comment above the class RecentSearches

--- src/com/spiralSpotManagement/SearchModule/RecentSearches.java
+++ src/com/spiralSpotManagement/SearchModule/RecentSearches.java
@@ -9,23 +9,34 @@ import java.util.ArrayList;
 import java.io.*;
 import java.sql.*;
 
+/*
+  @author Names: - MUGISHA ISAAC
+                 - ISHIMWE PAULINE
+  final date: on 5/02/2021
+  comment: This is a class called RecentSearches containing a method called viewAll which takes a connection of database
+           as its parameter and selects all  recent searches from searchHistory table. this method reverses those searched_queries
+           so as to order them by latest. it also checks if there is duplicate of queries and then returns only one if it found many same values on the same date.
+           The method also prints only 10 values.
+  So, this was our task, just to print the recent searched_queries.
+           Thank you!!!
+*/
 public class RecentSearches {
     public static void viewAll(Connection con) throws SQLException {
-        List resultArray = new ArrayList<String>();
-        // this will hold the result from db in array to display recent searches
-        // you will iterate from last elt and you display 10 searches at most
+        ArrayList<String> resultArray = new ArrayList<String>();
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select distinct searched_query,search_date from searchHistory where user_id =1 LIMIT 10");
+        ResultSet rs = stmt.executeQuery(
+                "select distinct searched_query,search_date from searchHistory where user_id =1 LIMIT 10");
         while (rs.next()) {
-            String searcheq = rs.getString("searched_query");
-            String madate = rs.getString("search_date");
-            String concat = searcheq + " on " + madate;
-            resultArray.add(concat);
+            String searchedQuery = rs.getString("searched_query");
+            String queryDate = rs.getString("search_date");
+            String concatenatedString = "\n"+ searchedQuery + " on " + queryDate;
+            System.out.println("\n");
+            resultArray.add(concatenatedString);
         }
+        System.out.println("\n");
         Collections.reverse(resultArray);
         System.out.println(resultArray);
     }
-
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -38,7 +49,7 @@ public class RecentSearches {
                 System.out.println("\n\n");
                 System.out.println("Review your recent searches\n");
                 System.out.println("---------------------------");
-                System.out.println("1.view all recent searches \n 2.Exit \n");
+                System.out.println(" 1.view all recent searches \n 2.Exit \n");
                 System.out.println("Enter your choice");
                 choice = Integer.parseInt(reader.readLine());
 
