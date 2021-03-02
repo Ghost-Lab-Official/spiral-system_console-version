d693fb40e794275edd13606163c4abe740707a65
Rwagapfizi Igor Roggy <rwagapfizi10@gmail.com>
2021-03-02 20:26:39 +0200

    added spot ratings

---
+++ src/com/spiralSpotManagement/Server/Controllers/SpotController/SpotRatingController.java
@@ -0,0 +1,40 @@
+
+/*
+        @author : Manzi Gusave
+        @Description: the controller is for the Spot Ratings
+ */
+
+package com.spiralSpotManagement.Server.Controllers.SpotController;
+
+import com.spiralSpotManagement.Server.Controllers.SpotCategoryControllers.SpotCategoryActions;
+import com.spiralSpotManagement.Server.Model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpotRatingController {
+    public  List<Object> mainSpotController(RequestBody requestBody)throws Exception{
+        String action = requestBody.getAction();
+        List<Object> usersObject = new ArrayList<>();
+        switch (action){
+            case "register":
+                ResponseStatus registeredSpotRating= new SpotRatingActions().createSpotRating((SpotRatings) requestBody.getObject());
+                usersObject.add((Object) registeredSpotRating);
+                return  usersObject;
+
+            case "update":
+                ResponseStatus updatedSpotRating= new SpotRatingActions().updateTheSpotRating((SpotRatings) requestBody.getObject());
+                usersObject.add((Object) updatedSpotRating);
+                return  usersObject;
+
+//            case "updateStatus":
+//                ResponseStatus updatedSpotRatingStatus = new SpotRatingActions().updateTheSpotRatingStatus((SpotRatings) requestBody.getObject());
+//                usersObject.add((Object) updatedSpotRatingStatus);
+//
+//                return  usersObject;
+
+            default:
+                return usersObject;
+        }
+    }
+}
