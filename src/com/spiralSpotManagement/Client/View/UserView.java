9175fb8e032541312bfd571ab39dd186756e8c31
Annabelle-anna <inezannabelle@gmail.com>
2021-03-04 22:00:59 +0200

    Last Updated UI

--- src/com/spiralSpotManagement/Client/View/UserView.java
+++ src/com/spiralSpotManagement/Client/View/UserView.java
@@ -65,14 +65,28 @@ public class UserView {
         requestBody.setObject(null);
 
         ResponseBody responseBody = new ClientServerConnector().ConnectToServer(requestBody);
-        System.out.println("\t\t\t List of Users\t\n");
-        System.out.println("\t ID \t first name \t last name \t user name \t gender \t email \t birth date \t location\n ");
+        System.out.println("\t\t\t\t LIST OF USERS\t");
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
+        System.out.println(String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |","#Id ","First name", "Last name","Username","Email","Gender","User category","Birth date","User Location"));
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
         for (Object response: responseBody.getResponse()){
             User user = (User) response;
 
-            System.out.println("\t "+ user.getUserId() + "\t\t" + user.getFirstName()+" \t\t "+user.getLastName() + "\t\t"
-            + user.getUserName() + "\t\t" + user.getGender() + "\t\t" + user.getEmail() + "\t\t" + user.getBirthDate() + "\t\t"
-            + user.getLocation());
+//            System.out.println("\t "+ user.getUserId() + "\t\t" + user.getFirstName()+" \t\t "+user.getLastName() + "\t\t"
+//            + user.getUserName() + "\t\t" + user.getGender() + "\t\t" + user.getEmail() + "\t\t" + user.getBirthDate() + "\t\t"
+//            + user.getLocation());
+            System.out.println(
+            String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |",
+                    user.getUserId(),
+                    user.getFirstName(),
+                    user.getLastName(),
+                    user.getUserName(),
+                    user.getEmail(),
+                    user.getGender(),
+                    user.getBirthDate(),
+                    user.getUserCategory(),
+                    user.getLocation())
+            );
         }
     }
     public void selectUserById() throws Exception{
@@ -87,13 +101,31 @@ public class UserView {
         ResponseBody responseBody = new ClientServerConnector().ConnectToServer(requestBody);
 
 
-        System.out.println("\t ID \t first name \t last name \t user name \t gender \t email \t birth date \t location\n ");
+//        System.out.println("\t ID \t first name \t last name \t user name \t gender \t email \t birth date \t location\n ");
+
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
+        System.out.println(String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |","#Id ","First name", "Last name","Username","Email","Gender","User category","Birth date","User Location"));
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
+
         for (Object response: responseBody.getResponse()){
             User users = (User) response;
             System.out.println("ON "+users.getUserName()+"'s Desk");
-            System.out.println("\t "+ users.getUserId() + "\t\t" + users.getFirstName()+" \t\t "+users.getLastName() + "\t\t"
-                    + users.getUserName() + "\t\t" + users.getGender() + "\t\t" + users.getEmail() + "\t\t" + users.getBirthDate() + "\t\t"
-                    + users.getLocation());
+//            System.out.println("\t "+ users.getUserId() + "\t\t" + users.getFirstName()+" \t\t "+users.getLastName() + "\t\t"
+//                    + users.getUserName() + "\t\t" + users.getGender() + "\t\t" + users.getEmail() + "\t\t" + users.getBirthDate() + "\t\t"
+//                    + users.getLocation());
+
+            System.out.println(
+                    String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |",
+                            users.getUserId(),
+                            users.getFirstName(),
+                            users.getLastName(),
+                            users.getUserName(),
+                            users.getEmail(),
+                            users.getGender(),
+                            users.getUserCategory(),
+                            users.getBirthDate(),
+                            users.getLocation())
+            );
         }
     }
     public void getUserProfile() throws Exception{
@@ -108,13 +140,24 @@ public class UserView {
         ResponseBody responseBody = new ClientServerConnector().ConnectToServer(requestBody);
 
 
-        System.out.println("\t ID \t first name \t last name \t user name \t gender \t email \t birth date \t location\n ");
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
+        System.out.println(String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |","#Id ","First name", "Last name","Username","Email","Gender","User category","Birth date","User Location"));
+        System.out.format("+-------+-----------------+-----------------+---------------------------+---------------------------+--------------+-----------------+---------------------------+-----------------+%n");
         for (Object response: responseBody.getResponse()){
             User users = (User) response;
             System.out.println("ON "+users.getUserName()+"'s Desk");
-            System.out.println("\t "+ users.getUserId() + "\t\t" + users.getFirstName()+" \t\t "+users.getLastName() + "\t\t"
-                    + users.getUserName() + "\t\t" + users.getGender() + "\t\t" + users.getEmail() + "\t\t" + users.getBirthDate() + "\t\t"
-                    + users.getLocation());
+            System.out.println(
+                    String.format("| %5s | %-15s | %-15s | %-25s | %-25s | %-12s | %-15s | %-25s | %-15s |",
+                            users.getUserId(),
+                            users.getFirstName(),
+                            users.getLastName(),
+                            users.getUserName(),
+                            users.getEmail(),
+                            users.getGender(),
+                            users.getUserCategory(),
+                            users.getBirthDate(),
+                            users.getLocation())
+            );
         }
     }
     public void registerUser()throws Exception{
